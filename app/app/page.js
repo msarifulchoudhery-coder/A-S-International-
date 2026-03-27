971b2c216caff26073e12b0d0babdc700b11c0fb
msarifulchoudhery-coder <msarifulchoudhery@gmail.com>
2026-03-28 01:02:51 +0400

    Create page.js

---
+++ app/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{textAlign:"center"}}>
+      <h1>A&S International LLC</h1>
+      <p>We sell safety equipment</p>
+    </div>
+  );
+}
